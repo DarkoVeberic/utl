0fc5e966bc7a3af922f3e4a5d55b19b32ac2b70b
Darko Veberic <darko.veberic@ijs.si>
2014-04-15 15:13:24 +0200

    Clear() not needed anymore

--- RootFile/TestClass.h
+++ RootFile/TestClass.h
@@ -16,15 +16,6 @@ public:
   double fDouble;
   std::vector<int> fVec;
 
-  // the only required method
-  void
-  Clear()
-  {
-    fInt = 0;
-    fDouble = 0;
-    fVec.clear();
-  }
-
   ClassDefNV(TestClass, 1);
 };
 
