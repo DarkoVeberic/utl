867b4d914fd7439a1acaa0f7c8dc26c13001c87a
Darko Veberic <darko.veberic@ijs.si>
2014-04-29 00:12:36 +0200

    raii

--- RootFile/SaveCurrentTDirectory.h
+++ RootFile/SaveCurrentTDirectory.h
@@ -4,6 +4,9 @@
 /*
   GPLv2 and 2C-BSD
   Copyright (c) Darko Veberic, 2014
+
+  RAII for gDirectory
+  http://en.wikipedia.org/wiki/Resource_Acquisition_Is_Initialization
 */
 
 #include <TDirectory.h>
