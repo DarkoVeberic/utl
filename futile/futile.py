c72b937d6cf7a34b69f388ec00cde25e0fdbe616
Darko Veberic <darko.veberic@kit.edu>
2026-06-02 20:44:04 +0200

    update and fix of pipe wrapper

--- futile/futile.py
+++ futile/futile.py
@@ -1,31 +1,55 @@
-def which(program):
-    import os
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-    return None
+#!/usr/bin/env python
+
+__author__ = ("Darko Veberic", "Philipp Meder")
+__copyright__ = "Copyright (C) 2021 Darko Veberic"
+__license__ = "GPLv3"
+__version__ = "1.2"
 
 
-def open_pipe(command, mode='r', buff=1024*1024):
+def open_pipe(command, mode="r", buff=1024*1024):
+    """ Open a pipe to a subprocess specified by 'command'.
+    Using a wrapper around Popen instead of returning Popen.stdin/stdout
+    SHOULD result in correct working 'with'-statements (waiting for the
+    process to finish). """
+
+    text = ("b" not in mode) or ("t" in mode)
     import subprocess
     import signal
-    if 'r' in mode:
-        return subprocess.Popen(command, shell=True, bufsize=buff,
-                                stdout=subprocess.PIPE,
-                                preexec_fn=lambda: signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-                               ).stdout
-    elif 'w' in mode:
-        return subprocess.Popen(command, shell=True, bufsize=buff,
-                                stdin=subprocess.PIPE).stdin
+
+    class PopenWrapper(subprocess.Popen):
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            if self.stdin:
+                self.stdin.close()
+            self.wait()
+            return False
+
+        def __iter__(self):
+            return iter(self.stdout or self.stdin)
+
+        def __next__(self):
+            return next(self.stdout or self.stdin)
+
+        def __getattribute__(self, __name: str):
+            try:
+                return super().__getattribute__(__name)
+            except AttributeError:
+                try:
+                    return self.stdin.__getattribute__(__name)
+                except (AttributeError, TypeError):
+                    return self.stdout.__getattribute__(__name)
+
+    if "r" in mode:
+        return PopenWrapper(
+            command, shell=True, bufsize=buff, text=text, stdout=subprocess.PIPE,
+            preexec_fn=lambda: signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+        )
+    if "w" in mode:
+        return PopenWrapper(
+            command, shell=True, bufsize=buff, text=text, stdin=subprocess.PIPE
+        )
     return None
 
 
@@ -34,63 +58,66 @@ PROCESS = 1
 PARALLEL = 2
 
 
-WHICH_BZIP2 = which("bzip2")
-WHICH_PBZIP2 = which("pbzip2")
-
 def open_bz2(filename, mode='r', buff=1024*1024, external=PARALLEL):
-    if external == None or external == NORMAL:
+    if external == NORMAL:
+        # the single-letter mode of bz2.open is by default binary, change it to correspond to the built-in open()
+        m = mode
+        if 'b' not in m and 't' not in m:
+            m += 't'
         import bz2
-        return bz2.BZ2File(filename, mode, buff)
-    elif external == PROCESS:
-        if not WHICH_BZIP2:
+        return bz2.open(filename, m)
+    import shutil
+    if external == PROCESS:
+        if not shutil.which("bzip2"):
             return open_bz2(filename, mode, buff, NORMAL)
         if 'r' in mode:
             return open_pipe("bzip2 -dc " + filename, mode, buff)
-        elif 'w' in mode:
+        if 'w' in mode:
             return open_pipe("bzip2 >" + filename, mode, buff)
-    elif external == PARALLEL:
-        if not WHICH_PBZIP2:
+    if external == PARALLEL:
+        if not shutil.which("pbzip2"):
             return open_bz2(filename, mode, buff, PROCESS)
         if 'r' in mode:
             return open_pipe("pbzip2 -dc " + filename, mode, buff)
-        elif 'w' in mode:
+        if 'w' in mode:
             return open_pipe("pbzip2 >" + filename, mode, buff)
-    return None
+    raise Exception("Unknown value of external option!")
 
-
-WHICH_GZIP = which("gzip")
-WHICH_PIGZ = which("pigz")
 
 def open_gz(filename, mode='r', buff=1024*1024, external=PARALLEL):
-    if external == None or external == NORMAL:
+    if external == NORMAL:
+        # the single-letter mode of gzip.open is by default binary, change it to correspond to the built-in open()
+        m = mode
+        if 'b' not in m and 't' not in m:
+            m += 't'
         import gzip
-        return gzip.GzipFile(filename, mode, buff)
-    elif external == PROCESS:
-        if not WHICH_GZIP:
+        return gzip.open(filename, m)
+    import shutil
+    if external == PROCESS:
+        if not shutil.which("gzip"):
             return open_gz(filename, mode, buff, NORMAL)
         if 'r' in mode:
             return open_pipe("gzip -dc " + filename, mode, buff)
-        elif 'w' in mode:
+        if 'w' in mode:
             return open_pipe("gzip >" + filename, mode, buff)
-    elif external == PARALLEL:
-        if not WHICH_PIGZ:
+    if external == PARALLEL:
+        if not shutil.which("pigz"):
             return open_gz(filename, mode, buff, PROCESS)
         if 'r' in mode:
             return open_pipe("pigz -dc " + filename, mode, buff)
-        elif 'w' in mode:
+        if 'w' in mode:
             return open_pipe("pigz >" + filename, mode, buff)
-    return None
+    raise Exception("Unknown value of external option!")
 
-
-WHICH_XZ = which("xz")
 
 def open_xz(filename, mode='r', buff=1024*1024, external=PARALLEL):
-    if WHICH_XZ:
+    import shutil
+    if shutil.which("xz"):
         if 'r' in mode:
             return open_pipe("xz -dc " + filename, mode, buff)
-        elif 'w' in mode:
+        if 'w' in mode:
             return open_pipe("xz >" + filename, mode, buff)
-    return None
+    raise Exception("xz not found!")
 
 
 def zopen(filename, mode='r', buff=1024*1024, external=PARALLEL):
@@ -98,28 +125,25 @@ def zopen(filename, mode='r', buff=1024*1024, external=PARALLEL):
     Open pipe, zipped, or unzipped file automagically
 
     # external == 0: normal zip libraries
-    # external == 1: (zcat, gzip) or (bzcat, bzip2)
-    # external == 2: (pigz -dc, pigz) or (pbzip2 -dc, pbzip2)
+    # external == 1: (zcat, gzip) or (bzcat, bzip2) or xz
+    # external == 2: pigz or pbzip2 or xz
     """
     if 'r' in mode and 'w' in mode:
         return None
     if filename.startswith('!'):
         return open_pipe(filename[1:], mode, buff)
-    elif filename.endswith('.bz2'):
+    if filename.endswith('.bz2'):
         return open_bz2(filename, mode, buff, external)
-    elif filename.endswith('.gz'):
+    if filename.endswith('.gz'):
         return open_gz(filename, mode, buff, external)
-    elif filename.endswith('.xz'):
+    if filename.endswith('.xz'):
         return open_xz(filename, mode, buff, external)
-    else:
-        return open(filename, mode, buff)
-    return None
+    return open(filename, mode, buff)
 
 
 def pickle_loader(pklFile):
     """ Add iterators to the pickle file so it can be used as a object container """
-
-    import cPickle as pkl
+    import pickle as pkl
     try:
         while True:
             yield pkl.load(pklFile)
@@ -132,13 +156,62 @@ def nice_time(t):
 
     t is in seconds
     """
-
-    div = (60., 60., 24., 365.25)
+    div = (60, 60, 24, 365.25)
     unit = ('', 's', 'min', 'h', 'day', 'year')
     sign = 1 if t >= 0 else -1
-    tt = sign*t
-    for i in xrange(len(div)):
+    tt = sign * t
+    for i in range(len(div)):
         if tt < div[i]:
             break
         tt /= div[i]
     return "%.2f %s" % (sign*tt, unit[i+1])
+
+
+if __name__ == "__main__":
+
+    def compare(a, b):
+        if a != b:
+            print(f"a=|{a}| not equal to b=|{b}|")
+            exit(1)
+        else:
+            print("ok")
+
+    basename = "/tmp/futile-test"
+    text = "first line\n1 2 3 foo bar baz\n a b c\nZ 13 &^%$"
+    file = basename + ".out"
+    with open(file, "w") as f:
+        print("write..", flush=True, end="")
+        print(text, file=f)
+    with open(file, "r") as f:
+        print("read..compare..", flush=True, end="")
+        compare(text, f.read().rstrip("\n"))
+    import os
+    import pickle
+    os.remove(file)
+    for z in ["", ".gz", ".bz2", ".xz"]:
+        for e in (NORMAL, PROCESS, PARALLEL):
+            file = basename + ".out" + z
+            print(f"z=\"{z}\" e={e} write..", flush=True, end="")
+            with zopen(file, "w", external=e) as f:
+                print(text, file=f)
+            print("read..compare..", flush=True, end="")
+            with zopen(file, "r", external=e) as f:
+                compare(text, f.read().rstrip("\n"))
+            print("line iterator:")
+            with zopen(file, "r", external=e) as f:
+                for line in f:
+                    print(line, end="")
+            print()
+            os.remove(file)
+
+            # test pickling
+            file = basename + ".pkl" + z
+            print(f"z=\"{z}\" e={e} pickle..", flush=True, end="")
+            with zopen(file, "wb", external=e) as f:
+                pickle.dump(text, f)
+            print("unpickle..compare..", flush=True, end="")
+            with zopen(file, "rb", external=e) as f:
+                pkl_text = pickle.load(f)
+                compare(text, pkl_text.rstrip("\n"))
+            print()
+            os.remove(file)
